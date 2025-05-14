e6e11f9252748bf1e979e4a45d5e2028ec11dd7e
Tereneckla <Tereneckla@pm.me>
2025-05-14 14:20:55 +0200

    gathering professions by level

--- sim/core/professions.go
+++ sim/core/professions.go
@@ -7,18 +7,37 @@ import (
 	"github.com/WoWLegacySims/wotlk/sim/core/stats"
 )
 
+func (character *Character) GetMaxProfessionRank() int32 {
+	switch {
+	case character.Level >= 55:
+		return 5
+	case character.Level >= 40:
+		return 4
+	case character.Level >= 25:
+		return 3
+	case character.Level >= 10:
+		return 2
+	default:
+		return 1
+	}
+}
+
 // This is just the static bonuses. Most professions are handled elsewhere.
 func (character *Character) applyProfessionEffects() {
 	if character.HasProfession(proto.Profession_Mining) {
-		character.AddStat(stats.Stamina, 60)
+		bonus := []float64{3, 5, 7, 10, 30, 60}[character.GetMaxProfessionRank()]
+		character.AddStat(stats.Stamina, bonus)
 	}
 
 	if character.HasProfession(proto.Profession_Skinning) {
-		character.AddStats(stats.Stats{stats.MeleeCrit: 40, stats.SpellCrit: 40})
+		bonus := []float64{3, 6, 9, 12, 20, 40}[character.GetMaxProfessionRank()]
+		character.AddStats(stats.Stats{stats.MeleeCrit: bonus, stats.SpellCrit: bonus})
 	}
 
 	if character.HasProfession(proto.Profession_Herbalism) {
-		actionID := ActionID{SpellID: 55503}
+		dbc := LifebloodInfos[character.GetMaxProfessionRank()]
+		heal := dbc.Effects[0].BasePoints + 1
+		actionID := ActionID{SpellID: dbc.SpellID}
 		healthMetrics := character.NewHealthMetrics(actionID)
 
 		spell := character.RegisterSpell(SpellConfig{
@@ -31,7 +50,7 @@ func (character *Character) applyProfessionEffects() {
 				},
 			},
 			ApplyEffects: func(sim *Simulation, _ *Unit, _ *Spell) {
-				amount := (3600 + character.MaxHealth()*0.016) / 5
+				amount := heal + character.MaxHealth()*0.0032
 				StartPeriodicAction(sim, PeriodicActionOptions{
 					Period:   time.Second,
 					NumTicks: 5,
