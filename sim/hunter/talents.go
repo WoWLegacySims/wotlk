2fdcff44d4d91b4e0b528be345e1c506ba762839
Tereneckla <Tereneckla@pm.me>
2025-06-05 14:06:12 +0200

    nil check in readiness

--- sim/hunter/talents.go
+++ sim/hunter/talents.go
@@ -831,9 +831,13 @@ func (hunter *Hunter) registerReadinessCD() {
 			hunter.RapidFire.CD.Reset()
 			hunter.MultiShot.CD.Reset()
 			hunter.ArcaneShot.CD.Reset()
-			hunter.KillShot.CD.Reset()
+			if hunter.KillShot != nil {
+				hunter.KillShot.CD.Reset()
+			}
 			hunter.RaptorStrike.CD.Reset()
-			hunter.ExplosiveTrap.CD.Reset()
+			if hunter.ExplosiveTrap != nil {
+				hunter.ExplosiveTrap.CD.Reset()
+			}
 			if hunter.KillCommand != nil {
 				hunter.KillCommand.CD.Reset()
 			}
